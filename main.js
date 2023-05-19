15dd7030288fb6845851aa3e8a60ab9c149ada79
carlos gonzalez <cgonzalez990811@gmail.com>
2023-05-19 16:14:51 -0500

    agregando carrito de compras, no terminado aun

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarrIcon = document.querySelector('.navbar-shopping-cart');
+const showCarrito = document.querySelector('')
+
 
 
 
